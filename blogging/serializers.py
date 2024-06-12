f72f252508c78c366bbb17540667387ce880e1e5
PadmaBham <118145181+PadmaBham@users.noreply.github.com>
2024-06-11 20:38:40 -0700

    fixed error with serializer

--- blogging/serializers.py
+++ blogging/serializers.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
-
 from .models import Post
 
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['title', 'author', 'text', 'created_date', 'modified_date', 'published_date']
